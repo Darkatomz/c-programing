500f91508e44b106f91c225b98f63f18cdafa71a
Darkatomz <49082755+Darkatomz@users.noreply.github.com>
2020-06-05 22:58:11 +0100

    Update Fitness.h
    improved

--- Fitness.h
+++ Fitness.h
@@ -5,18 +5,28 @@
 class Fitness {
 
 private:
-/*	enum ChangeMass
-	{
-		Slow=1,Normal,Fast,ReallyFast
-	};*/
+	enum ChangeMass
+		{
+			Slow=1,Normal,Fast,ReallyFast
+		};
 
-/*	enum ActivityLevels
-	{
+	enum ActivityLevels
+		{
 		DoNothing = 14, OcassionalyMoveAround,AfewActivities,VeryActive
-	};*/
-/*	enum ChangeWeight {
+		};
+	enum ChangeWeight {
 		LooseWeight = 1, MaintainWeight,GainWeight
-	};*/
+		};
+
+	float CalculateMacroRatio(float kcal, float carbperc, float fatperc, float protienperc);
+	float DetermaineHowManyKcalRequired(float bodyweight, ActivityLevels activitylevel, std::string unit);//Calculated how many kcal you need(for either kg or lbs)
+	void DisplayInfo();
+	float ChangeBodyWeight(ChangeWeight Option, ChangeMass Speed);
+	std::string MeasurementYouWant();
+	ActivityLevels ConvertActivityLevelToOptions(char optionforactivity);
+	std::string MeasureMentUnitConverter(std::string Measurement);
+	void StartPromt1();
+	float* StoreMacroPercentages();
 	const float _slow{ 200.0f }, _normal{ 300.0f }, _fast{ 350.0f }, _reallyfast{ 500.0f };
 	float kcal{}, carbs{}, fat{}, protien{};
 	std::string User{};
@@ -29,26 +39,6 @@ private:
 	char UserOption{};
 
 public:
-	//remember to deleate this enum
-	enum ChangeWeight {
-		LooseWeight = 1, MaintainWeight, GainWeight
-	};
-	enum ActivityLevels
-	{
-		DoNothing = 14, OcassionalyMoveAround, AfewActivities, VeryActive
-	};
-	enum ChangeMass
-	{
-		Slow = 1, Normal, Fast, ReallyFast
-	};
-	float CalculateMacroRatio(float kcal,float carbperc, float fatperc, float protienperc);
-	float DetermaineHowManyKcalRequired(float bodyweight, ActivityLevels activitylevel,std::string unit);//Calculated how many kcal you need(for either kg or lbs)
-	void DisplayInfo();
-	float ChangeBodyWeight(ChangeWeight Option, ChangeMass Speed);
 	void Start();
-	std::string MeasurementYouWant();
-	ActivityLevels ConvertActivityLevelToOptions(char optionforactivity);
-	std::string MeasureMentUnitConverter(std::string Measurement);
-	void StartPromt1();
-	float* StoreMacroPercentages();
+	
 };
